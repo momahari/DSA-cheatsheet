9db559028257ff798cd8d3038fcbc31f2d23dc61
Mahari Mohamed <mohamedmahari566@gmail.com>
2025-08-22 23:17:09 +0100

    add circular_queue class

--- data structures/Stack_Queue.py
+++ data structures/Stack_Queue.py
@@ -70,6 +70,59 @@ class Queue:
     def clear_queue(self):
         return self.buffer.clear()
 
+
+class CircularQueue:
+
+    def __init__(self, k: int):
+        self.k = k
+        self.queue = [None] * k
+        self.head = 0
+        self.tail = 0
+        self.count = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.queue[self.tail] = value
+        self.tail = (self.tail + 1) % self.k
+        self.count += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+
+        self.queue[self.head] = None
+        self.head = (self.head + 1) % self.k
+        self.count -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.queue[self.head]
+
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.queue[(self.tail - 1 + self.k) % self.k]
+
+    def isEmpty(self) -> bool:
+        return self.count == 0
+
+    def isFull(self) -> bool:
+        return self.count == self.k
+
+
+# Your CircularQueue object will be instantiated and called as such:
+# obj = CircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
+
 if __name__ == "__main__":
     s = Stack()
     s.push('Moroco')
